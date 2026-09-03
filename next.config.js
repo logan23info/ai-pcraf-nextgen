0daf000122c32aead8aec71b63476b1e66f7a1f3
Logan <mploganathan23@gmail.com>
2026-09-03 11:52:20 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverComponentsExternalPackages: ['pdfkit'] }
+  experimental: {
+    serverComponentsExternalPackages: ['pdfkit', 'pdf-parse', 'mammoth']
+  }
 }
 module.exports = nextConfig
